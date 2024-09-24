7e8153cb4c2e23cedde843f67d36de9af6d7289f
Yash Sehgal <yash.sehgal@rocketium.com>
2024-09-24 13:57:29 +0530

    Create a playground layout for projects page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import ProjectsPage from "./pages/projects-page";
+
+type RouteType = {
+  route: string; // eg, route: projects
+  component: React.ReactNode; // eg, component: <ProjectsPage />
+};
+
+const ROUTES: RouteType[] = [
+  { route: "projects", component: <ProjectsPage /> },
+];
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        {ROUTES.map(({ route, component }, index) => {
+          return <Route key={index} path={`/${route}`} element={component} />;
+        })}
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/projects-page.tsx
@@ -0,0 +1,9 @@
+const ProjectsPage = () => {
+  return (
+    <div className="page projects-page">
+      <header className="page-header bg-white w-full h-14"></header>
+    </div>
+  );
+};
+
+export default ProjectsPage;
